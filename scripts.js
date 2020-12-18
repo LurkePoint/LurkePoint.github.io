3906a2ba957400c2de0121e581e3eb10bf70e8b4
LurkePoint <49284978+LurkePoint@users.noreply.github.com>
2020-12-18 16:19:11 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,12 +4,3 @@ $('.burger').on('click', function (e) {
     $('.menu').toggleClass('menu-active')
     $('body').toggleClass('lock')
 })
-
-$('.mobile div').on('click', () => {
-    $('.mobile div').toggleClass('active');
-    $('.mobile nav').toggleClass('open');
-    $('.mobile nav ul').toggleClass('show');
-});
-for (let a = 1; a <= $(".mobile ul li").length; a++){ 
-    $(".mobile ul li:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");    
-}
